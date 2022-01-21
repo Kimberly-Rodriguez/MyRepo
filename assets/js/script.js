441fc0f2cc7a2bd3bb9a5b59a49556bff33b8499
kimberly Rodriguez <kimberlyrodriguez@kimberlys-iMac.attlocal.net>
2022-01-21 14:52:59 -0800

    formSubmitHandler function for addEventListener that gathers/checks for values

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,28 @@
-//referencing id's
+//referencing id's from html document
 var userFormEl = document.querySelector('#user-form');
 var languageButtonsEl = document.querySelector('#language-buttons');
 var nameInputEl = document.querySelector('#username');
 var repoContainerEl = document.querySelector('#repos-container');
 var repoSearchTerm = document.querySelector('#repo-search-term');
+
+
+
+let formSubmitHandler = function(e){
+  e.preventdefult();
+
+  let username = nameIput.value.trim();
+
+  if (username) {
+    getUserRepos(username);
+    repoContainerEl.textContent = '';
+    nameInputEl.value = '';
+    } else {
+      alert('please enter a Github username');
+    }
+};
+
+
+
+
+
+userFormEl.addEventListener('submit', formSubmitHandler);
