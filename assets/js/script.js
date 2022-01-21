e47948bd9701559735b62517b6dea216bd3bc52b
kimberly Rodriguez <kimberlyrodriguez@kimberlys-iMac.attlocal.net>
2022-01-21 14:41:56 -0800

    referencing id's by using querySelector

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+//referencing id's
+var userFormEl = document.querySelector('#user-form');
+var languageButtonsEl = document.querySelector('#language-buttons');
+var nameInputEl = document.querySelector('#username');
+var repoContainerEl = document.querySelector('#repos-container');
+var repoSearchTerm = document.querySelector('#repo-search-term');
